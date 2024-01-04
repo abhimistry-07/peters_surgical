88c043600f342c52476f88b2f148792a6cc22078
Abhishek <mistryabhishek2000@gmail.com>
2024-01-04 21:56:28 +0530

    Purchase section added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,78 @@
 import React from "react";
 import Navbar from "./Navbar";
+import styled from "styled-components";
+import PurchasedSection from "./PurchasedSection";
 
 function Main() {
   return (
     <div>
       <Navbar />
+      <DATE>
+        <div className="innerDiv">
+          <p>
+            Show Tline:
+            <span>Mar’23 - Apr’23</span>
+          </p>
+          <div className="line"></div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="8"
+            height="6"
+            viewBox="0 0 8 6"
+            fill="none"
+          >
+            <path
+              d="M2.90573 5.00001L0.305725 2.40001C-0.0109418 2.08335 -0.0816084 1.72101 0.093725 1.31301C0.269058 0.905012 0.581392 0.700679 1.03073 0.700012H6.18072C6.63072 0.700012 6.94339 0.904345 7.11872 1.31301C7.29406 1.72168 7.22306 2.08401 6.90573 2.40001L4.30572 5.00001C4.20572 5.10001 4.09739 5.17501 3.98073 5.22501C3.86406 5.27501 3.73906 5.30001 3.60573 5.30001C3.47239 5.30001 3.34739 5.27501 3.23073 5.22501C3.11406 5.17501 3.00573 5.10001 2.90573 5.00001Z"
+              fill="black"
+              fill-opacity="0.84"
+            />
+          </svg>
+        </div>
+      </DATE>
+      <PurchasedSection />
+      
     </div>
   );
 }
+
+const DATE = styled.div`
+  margin-left: 895px;
+  margin-top: 24px;
+  height: 32px;
+  box-shadow: 0px 0.599px 5.32px 0px rgba(0, 0, 0, 0.01),
+    0px 2.01px 17.869px 0px rgba(0, 0, 0, 0.01),
+    0px 9px 80px 0px rgba(0, 0, 0, 0.02);
+
+  .innerDiv {
+    display: flex;
+    height: 32px;
+    padding: 10px 12px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    width: max-content;
+    border: 1px solid #02ab6c;
+    border-radius: 8px;
+
+    p {
+      color: #0000007a;
+      font-family: HelveticaNeue;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+
+      span {
+        color: #02ab6c;
+      }
+    }
+
+    .line {
+      width: 1px;
+      height: 16px;
+      background: linear-gradient(0deg, #02ab6c 0%, #02ab6c 100%), #fff;
+    }
+  }
+`;
 
 export default Main;
---
+++ src/components/PurchasedSection.jsx
@@ -0,0 +1,302 @@
+import React from "react";
+import styled from "styled-components";
+
+function PurchasedSection() {
+  return (
+    <PurchaseContainer>
+      <div className="container">
+        <div className="subContainer1">
+          <p>Purchased goods and Services</p>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 14 14"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_1_76)">
+                <path
+                  d="M7.83333 1C11.0551 1 13.6667 3.61158 13.6667 6.83333C13.6667 10.0551 11.0551 12.6667 7.83333 12.6667C4.61158 12.6667 2 10.0551 2 6.83333C2 3.61158 4.61158 1 7.83333 1ZM7.83333 2.16667C6.59566 2.16667 5.40867 2.65833 4.5335 3.5335C3.65833 4.40867 3.16667 5.59566 3.16667 6.83333C3.16667 8.07101 3.65833 9.258 4.5335 10.1332C5.40867 11.0083 6.59566 11.5 7.83333 11.5C9.07101 11.5 10.258 11.0083 11.1332 10.1332C12.0083 9.258 12.5 8.07101 12.5 6.83333C12.5 5.59566 12.0083 4.40867 11.1332 3.5335C10.258 2.65833 9.07101 2.16667 7.83333 2.16667ZM7.8275 5.66667C8.153 5.66667 8.41667 5.93033 8.41667 6.25583V9.24483C8.52787 9.30904 8.61477 9.40815 8.66391 9.52678C8.71305 9.64542 8.72167 9.77695 8.68843 9.90098C8.6552 10.025 8.58197 10.1346 8.48009 10.2128C8.37822 10.291 8.25341 10.3333 8.125 10.3333H7.83917C7.7618 10.3333 7.68518 10.3181 7.6137 10.2885C7.54222 10.2589 7.47727 10.2155 7.42256 10.1608C7.36785 10.1061 7.32446 10.0411 7.29485 9.96963C7.26524 9.89815 7.25 9.82154 7.25 9.74417V6.83333C7.09529 6.83333 6.94692 6.77188 6.83752 6.66248C6.72813 6.55308 6.66667 6.40471 6.66667 6.25C6.66667 6.09529 6.72813 5.94692 6.83752 5.83752C6.94692 5.72813 7.09529 5.66667 7.25 5.66667H7.8275ZM7.83333 3.91667C7.98804 3.91667 8.13642 3.97813 8.24581 4.08752C8.35521 4.19692 8.41667 4.34529 8.41667 4.5C8.41667 4.65471 8.35521 4.80308 8.24581 4.91248C8.13642 5.02188 7.98804 5.08333 7.83333 5.08333C7.67862 5.08333 7.53025 5.02188 7.42086 4.91248C7.31146 4.80308 7.25 4.65471 7.25 4.5C7.25 4.34529 7.31146 4.19692 7.42086 4.08752C7.53025 3.97813 7.67862 3.91667 7.83333 3.91667Z"
+                  fill="black"
+                  fill-opacity="0.8"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_1_76">
+                  <rect width="14" height="14" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </div>
+        </div>
+        <p className="smallText">contribution to Scope3</p>
+        <div className="subContainer2">
+          <div className="sub3">
+            <p>441510.9 CO2e</p>
+            <div className="sub4">
+              <p>25%YOY↑</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="subContainer1">
+          <p>Purchased good and services to revenue ratio</p>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 14 14"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_1_76)">
+                <path
+                  d="M7.83333 1C11.0551 1 13.6667 3.61158 13.6667 6.83333C13.6667 10.0551 11.0551 12.6667 7.83333 12.6667C4.61158 12.6667 2 10.0551 2 6.83333C2 3.61158 4.61158 1 7.83333 1ZM7.83333 2.16667C6.59566 2.16667 5.40867 2.65833 4.5335 3.5335C3.65833 4.40867 3.16667 5.59566 3.16667 6.83333C3.16667 8.07101 3.65833 9.258 4.5335 10.1332C5.40867 11.0083 6.59566 11.5 7.83333 11.5C9.07101 11.5 10.258 11.0083 11.1332 10.1332C12.0083 9.258 12.5 8.07101 12.5 6.83333C12.5 5.59566 12.0083 4.40867 11.1332 3.5335C10.258 2.65833 9.07101 2.16667 7.83333 2.16667ZM7.8275 5.66667C8.153 5.66667 8.41667 5.93033 8.41667 6.25583V9.24483C8.52787 9.30904 8.61477 9.40815 8.66391 9.52678C8.71305 9.64542 8.72167 9.77695 8.68843 9.90098C8.6552 10.025 8.58197 10.1346 8.48009 10.2128C8.37822 10.291 8.25341 10.3333 8.125 10.3333H7.83917C7.7618 10.3333 7.68518 10.3181 7.6137 10.2885C7.54222 10.2589 7.47727 10.2155 7.42256 10.1608C7.36785 10.1061 7.32446 10.0411 7.29485 9.96963C7.26524 9.89815 7.25 9.82154 7.25 9.74417V6.83333C7.09529 6.83333 6.94692 6.77188 6.83752 6.66248C6.72813 6.55308 6.66667 6.40471 6.66667 6.25C6.66667 6.09529 6.72813 5.94692 6.83752 5.83752C6.94692 5.72813 7.09529 5.66667 7.25 5.66667H7.8275ZM7.83333 3.91667C7.98804 3.91667 8.13642 3.97813 8.24581 4.08752C8.35521 4.19692 8.41667 4.34529 8.41667 4.5C8.41667 4.65471 8.35521 4.80308 8.24581 4.91248C8.13642 5.02188 7.98804 5.08333 7.83333 5.08333C7.67862 5.08333 7.53025 5.02188 7.42086 4.91248C7.31146 4.80308 7.25 4.65471 7.25 4.5C7.25 4.34529 7.31146 4.19692 7.42086 4.08752C7.53025 3.97813 7.67862 3.91667 7.83333 3.91667Z"
+                  fill="black"
+                  fill-opacity="0.8"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_1_76">
+                  <rect width="14" height="14" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </div>
+        </div>
+        <div className="subContainer2">
+          <div
+            className="sub3"
+            style={{
+              borderLeft: "6px solid #FFC400",
+              // border: "1px solid #FFC400",
+              background:
+                "linear-gradient(0deg, rgba(255, 196, 0, 0.04) 0%, rgba(255, 196, 0, 0.04) 100%), #FFF",
+            }}
+          >
+            <p>323586.2 CO2e</p>
+            <div className="sub4">
+              <p>24%YOY↓</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="subContainer1">
+          <p>Category-1</p>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 14 14"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_1_76)">
+                <path
+                  d="M7.83333 1C11.0551 1 13.6667 3.61158 13.6667 6.83333C13.6667 10.0551 11.0551 12.6667 7.83333 12.6667C4.61158 12.6667 2 10.0551 2 6.83333C2 3.61158 4.61158 1 7.83333 1ZM7.83333 2.16667C6.59566 2.16667 5.40867 2.65833 4.5335 3.5335C3.65833 4.40867 3.16667 5.59566 3.16667 6.83333C3.16667 8.07101 3.65833 9.258 4.5335 10.1332C5.40867 11.0083 6.59566 11.5 7.83333 11.5C9.07101 11.5 10.258 11.0083 11.1332 10.1332C12.0083 9.258 12.5 8.07101 12.5 6.83333C12.5 5.59566 12.0083 4.40867 11.1332 3.5335C10.258 2.65833 9.07101 2.16667 7.83333 2.16667ZM7.8275 5.66667C8.153 5.66667 8.41667 5.93033 8.41667 6.25583V9.24483C8.52787 9.30904 8.61477 9.40815 8.66391 9.52678C8.71305 9.64542 8.72167 9.77695 8.68843 9.90098C8.6552 10.025 8.58197 10.1346 8.48009 10.2128C8.37822 10.291 8.25341 10.3333 8.125 10.3333H7.83917C7.7618 10.3333 7.68518 10.3181 7.6137 10.2885C7.54222 10.2589 7.47727 10.2155 7.42256 10.1608C7.36785 10.1061 7.32446 10.0411 7.29485 9.96963C7.26524 9.89815 7.25 9.82154 7.25 9.74417V6.83333C7.09529 6.83333 6.94692 6.77188 6.83752 6.66248C6.72813 6.55308 6.66667 6.40471 6.66667 6.25C6.66667 6.09529 6.72813 5.94692 6.83752 5.83752C6.94692 5.72813 7.09529 5.66667 7.25 5.66667H7.8275ZM7.83333 3.91667C7.98804 3.91667 8.13642 3.97813 8.24581 4.08752C8.35521 4.19692 8.41667 4.34529 8.41667 4.5C8.41667 4.65471 8.35521 4.80308 8.24581 4.91248C8.13642 5.02188 7.98804 5.08333 7.83333 5.08333C7.67862 5.08333 7.53025 5.02188 7.42086 4.91248C7.31146 4.80308 7.25 4.65471 7.25 4.5C7.25 4.34529 7.31146 4.19692 7.42086 4.08752C7.53025 3.97813 7.67862 3.91667 7.83333 3.91667Z"
+                  fill="black"
+                  fill-opacity="0.8"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_1_76">
+                  <rect width="14" height="14" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </div>
+        </div>
+        <p className="smallText">contribution to Scope3</p>
+        <div className="subContainer2">
+          <div
+            className="sub3"
+            style={{
+              borderLeft: "6px solid #3BB85E",
+              background:
+                "linear-gradient(0deg, rgba(59, 184, 94, 0.04) 0%, rgba(59, 184, 94, 0.04) 100%), #FFF",
+            }}
+          >
+            <p>32% of Scope3</p>
+            <div className="sub4">
+              <p>25%YOY↑</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container4">
+        <div className="top">
+          <p>
+            Total number of
+            <br />
+            reached suppliers
+          </p>
+        </div>
+        <div className="bottom">
+          <p>143</p>
+        </div>
+      </div>
+    </PurchaseContainer>
+  );
+}
+
+const PurchaseContainer = styled.div`
+  display: flex;
+  width: 1036px;
+  gap: 20px;
+  margin-left: 30px;
+  margin-top: 43px;
+
+  .container {
+    padding: 24px 20px;
+    display: flex;
+    width: 267px;
+    height: 134px;
+    flex-direction: column;
+    align-items: flex-start;
+    flex-shrink: 0;
+    /* border: 1px solid red; */
+    border-radius: 12px;
+    border: 1px solid #ebebeb;
+    background: #fff;
+    box-shadow: 0px 0.599px 5.32px 0px rgba(0, 0, 0, 0.01),
+      0px 2.01px 17.869px 0px rgba(0, 0, 0, 0.01),
+      0px 9px 80px 0px rgba(0, 0, 0, 0.02);
+
+    .subContainer1 {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      /* height: 64px; */
+      /* margin-left: 20px; */
+
+      p {
+        margin-right: 8px;
+        color: #000000cc;
+        font-family: HelveticaNeue;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+      }
+    }
+
+    .smallText {
+      color: rgba(0, 0, 0, 0.67);
+      text-align: center;
+      font-family: Poppins;
+      font-size: 8px;
+      font-style: italic;
+      font-weight: 275;
+      line-height: normal;
+    }
+
+    .subContainer2 {
+      display: flex;
+      /* justify-content: flex-end; */
+      align-items: center;
+      align-self: stretch;
+      border-radius: 8px;
+      border: 1px solid #fff;
+
+      .sub3 {
+        margin-top: 17px;
+        display: flex;
+        padding: 9px;
+        /* padding: 9px 0px 7px 21px; */
+        align-items: center;
+        /* gap: 16px; */
+        align-self: stretch;
+        border-radius: 8px;
+        border-left: 6px solid #e03021;
+        background: linear-gradient(
+            0deg,
+            rgba(224, 48, 33, 0.04) 0%,
+            rgba(224, 48, 33, 0.04) 100%
+          ),
+          #fff;
+
+        p {
+          /* color: #FFF; */
+          text-align: center;
+          font-family: HelveticaNeue;
+          font-size: 15px;
+          font-style: normal;
+          font-weight: 400;
+          line-height: normal;
+        }
+
+        .sub4 {
+          display: flex;
+          width: 77px;
+          height: 28px;
+          flex-direction: column;
+          justify-content: center;
+          flex-shrink: 0;
+
+          p {
+            color: rgba(171, 2, 2, 0.67);
+            text-align: center;
+            font-family: HelveticaNeue;
+            font-size: 11px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: normal;
+          }
+        }
+      }
+    }
+  }
+
+  .container4 {
+    display: inline-flex;
+    flex-direction: column;
+    align-items: flex-start;
+    border-radius: 12px;
+    border: 1px solid #ebebeb;
+    background: rgba(2, 171, 108, 0.67);
+    box-shadow: 0px 0.599px 5.32px 0px rgba(0, 0, 0, 0.01),
+      0px 2.01px 17.869px 0px rgba(0, 0, 0, 0.01),
+      0px 9px 80px 0px rgba(0, 0, 0, 0.02);
+
+    .top {
+      display: flex;
+      height: 64px;
+      padding: 20px;
+      align-items: center;
+      gap: 10px;
+
+      p {
+        color: #fff;
+        font-family: HelveticaNeue;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 18px; /* 128.571% */
+      }
+    }
+
+    .bottom {
+      display: flex;
+      padding: 0px 20px 20px 20px;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      align-self: stretch;
+
+      p {
+        color: rgba(255, 255, 255, 0.84);
+        text-align: center;
+        font-family: HelveticaNeue;
+        font-size: 46px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+      }
+    }
+  }
+`;
+
+export default PurchasedSection;
