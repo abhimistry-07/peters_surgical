466ddb2a25311b1701051fadd7f6fb22ca3d536d
Abhishek <mistryabhishek2000@gmail.com>
2024-01-04 14:26:46 +0530

    Set up

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      HomePage
+      <h1
+        className="text-3xl 
+        underline
+        font-bold 
+      "
+      >
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default HomePage;
